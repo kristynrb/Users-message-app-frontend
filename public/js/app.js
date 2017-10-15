3cf2aa3ba1f5d591b9c93a4652ae01e441907840
Kristyn Bryan <kristynrb@gmail.com>
2017-10-15 14:48:47 -0400

    setting up form for user

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,24 @@
 const app = angular.module('OmniApp', []);
 
-app.controller('MyController', [ function(){
-    this.foo = 'bar';
+app.controller('MyController', ['$http', function($http){
+    this.foo = "bar";
+    const controller = this;
+
+    this.createUser = function(){
+      console.log("I'm inside createUser");
+      $http({
+        method: 'POST',
+        url: 'http://localhost:3000/users/',
+        data: {
+          username: this.username,
+          password: this.password
+        }
+      }).then(
+        function(response){
+          console.log(response);
+          console.log(req.body);
+      }, function(){
+          console.log('error');
+      });
+    }
 }]);
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.listen(3001, ()=>{
+    console.log('listening to port 3001');
+});
