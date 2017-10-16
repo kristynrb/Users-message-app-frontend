52b932cf5e0962c3ca9965606cb738fc15121a8a
Kristyn Bryan <kristynrb@gmail.com>
2017-10-16 00:50:27 -0400

    updating public message controller

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,42 @@ const app = angular.module('OmniApp', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   $locationProvider.html5Mode({enabled:true});
+
   $routeProvider.when('/user/new', {
     templateUrl: 'views/registration.html'
   });
+
   $routeProvider.when('/user/signIn', {
     templateUrl: 'views/sign-in.html'
   });
+
   $routeProvider.when('/messages/public', {
-    templateUrl: 'views/public-messages.html'
+    templateUrl: 'views/public-messages.html',
+    controller: function(){
+      controller = this;
+      this.publicMessages = "no public messages yet";
+
+      this.getPublicMessages = function($http){
+        ccontroller.publicMessages = "getPublicMessages was clicked";
+       $http({
+           method:'GET',
+           url:'localhost:3000/messages'
+       }).then(
+           function(response){
+               controller.publicMessages = response.data;
+           },
+           function(error){
+           }
+       );
+      }
+    },
+    controllerAs: 'publicMessageCtrl'
   });
+
   $routeProvider.when('/messages/private', {
     templateUrl: 'views/private-messages.html'
   });
+
   $routeProvider.when('/messages/new', {
     templateUrl: 'views/new-messages.html',
     controller: function(){
@@ -23,26 +47,3 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
       controllerAs: 'messageCtrl'
   });
 }]);
-
-// app.controller('MyController', ['$http', function($http){
-//     this.foo = "bar";
-//     const controller = this;
-//
-//     this.createUser = function(){
-//       console.log("I'm inside createUser");
-//       $http({
-//         method: 'POST',
-//         url: 'http://localhost:3000/users/',
-//         data: {
-//           username: this.username,
-//           password: this.password
-//         }
-//       }).then(
-//         function(response){
-//           console.log(response);
-//           console.log(req.body);
-//       }, function(){
-//           console.log('error');
-//       });
-//     }
-// }]);
