5a08d2bf340b776e0686b288c281e812fbe0b60a
Kristyn Bryan <kristynrb@gmail.com>
2017-10-15 23:11:20 -0400

    updated to ng-route. created registration view

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,30 @@
-const app = angular.module('OmniApp', []);
+const app = angular.module('OmniApp', ['ngRoute']);
 
-app.controller('MyController', ['$http', function($http){
-    this.foo = "bar";
-    const controller = this;
-
-    this.createUser = function(){
-      console.log("I'm inside createUser");
-      $http({
-        method: 'POST',
-        url: 'http://localhost:3000/users/',
-        data: {
-          username: this.username,
-          password: this.password
-        }
-      }).then(
-        function(response){
-          console.log(response);
-          console.log(req.body);
-      }, function(){
-          console.log('error');
-      });
-    }
-}]);
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+  $locationProvider.html5Mode({enabled:true});
+  $routeProvider.when('/user/new', {
+    templateUrl: 'views/registration.html'
+  })
+}])
+// app.controller('MyController', ['$http', function($http){
+//     this.foo = "bar";
+//     const controller = this;
+//
+//     this.createUser = function(){
+//       console.log("I'm inside createUser");
+//       $http({
+//         method: 'POST',
+//         url: 'http://localhost:3000/users/',
+//         data: {
+//           username: this.username,
+//           password: this.password
+//         }
+//       }).then(
+//         function(response){
+//           console.log(response);
+//           console.log(req.body);
+//       }, function(){
+//           console.log('error');
+//       });
+//     }
+// }]);
